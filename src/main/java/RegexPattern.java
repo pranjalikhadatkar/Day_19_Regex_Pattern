e152073df4356d9bca75f4946558cd1b4c95399a
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-02-24 17:48:14 +0530

    Added: user need to enter pre-defined password should have at least one upper case

--- src/main/java/RegexPattern.java
+++ src/main/java/RegexPattern.java
@@ -8,13 +8,13 @@ public class RegexPattern {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String password;
-        System.out.println(" enter password which contains minimum 8 characters: ");
+        System.out.println(" enter password which contains minimum 8 characters & should have at least 1 upper case: ");
         password = sc.nextLine();
         confirmpassword(password);
     }
 
     public static void confirmpassword(String password) {
-        Pattern pattern = Pattern.compile("^[A-Za-z0-9!@#$%^&*()+=?]{8,}$");
+        Pattern pattern = Pattern.compile("^(?=.*[A-Z])[A-Za-z]{8,}$");
         Matcher matcher = pattern.matcher(password);
         if (matcher.matches()) {
             System.out.println(" format of password is correct " + password);
